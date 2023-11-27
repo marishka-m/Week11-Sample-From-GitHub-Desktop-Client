755e74bd0535dea47e60616540c61b208cdd96ce
marishka-m <145405382+marishka-m@users.noreply.github.com>
2023-11-27 17:41:00 -0500

    Create SampleProgram2 Java
    This is a  sample program Java file made for this repository.

---
+++ SampleProgram2.java
@@ -0,0 +1,14 @@
+public class SampleProgram2 {
+
+    public static void main(String[] args) {
+        //Print our Hello world message
+        System.out.println("Hello, World!");
+
+        // Print numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+        // Print fun message
+        System.out.println("This is fun! It is my 2nd attempt");
+    }
+}
